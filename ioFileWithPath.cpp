7ccd59168785a5db45ab6b3b87ffc4c17c6c97b6
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-10 18:56:35 +0700

    membuka file dalam mode menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,3 +3,12 @@
 #include <string>
 using namespace std;
 
+int main() {
+    string baris;
+    string NamaFile;
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+}
+
