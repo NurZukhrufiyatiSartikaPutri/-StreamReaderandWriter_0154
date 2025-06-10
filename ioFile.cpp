7786ca53ab2a841d8dff591145be2a41f311b815
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-10 17:55:40 +0700

    unlimited loop untuk menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -10,4 +10,8 @@ int main() {
     // menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+     //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+    }
 }
