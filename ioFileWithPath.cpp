57038d54949c2f75588bd75db76bec9c594e9dad
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-06-10 18:49:56 +0700

    menambahkan library fstream, iostream, dan string

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -3,27 +3,3 @@
 #include <string>
 using namespace std;
 
-int main() {
-    string baris;
-    string NamaFile;
-    cout << "Masukkan Nama File : ";
-    cin >> NamaFile;
-    //membuka file dalam mode menulis.
-    ofstream outfile;
-    // menunjuk ke sebuah nama file
-    outfile.open(NamaFile + "txt", ios::out);
-    cout << ">= Menulis file \'q' untuk keluar" << endl;
-    //unlimited loop untuk menulis
-    while (true) {
-        cout << "- ";
-        //mendapatkan setiap karakter dalam satu baris
-        getline(cin, baris);
-        //loop akan berhenti jika anda masukkan karakter q
-        if (baris == "q") break;
-        //menulis dan memasukkan nilai dari 'baris' ke dalam ke dalam file 
-        outfile << baris << endl;
-    }
-    //selesai dalam menulis sekarang tutup filenya
-    outfile.close();
-    //Membuka file dalam mode membaca
-}
